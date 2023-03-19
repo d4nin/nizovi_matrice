e9ad8f4e716be7a49181221ccabc2ccf205ca1a4
Danin Sadzak <daninsadzak@gmail.com>
2023-03-19 13:12:46 +0100

    Zavrsen drugi (tacan)

--- 2.cpp
+++ 2.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main() {
-    
+    int n,p;
+    cout << "Koliki niz?\n";
+    cin >> n;
+    int niz[n];
+    cout << "Broj p?\n";
+    cin >> p;
+    cout << "Unesite clanove niza:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    int br(0);
+    for(int i = 0; i < n; i++) {
+        if(niz[i] % 2 == 0 && niz[i] > p) br++;
+    }
+    cout << "Broj ponavljanja parnih brojeva vecih od p: " << br;
     return 0;
 }
+/*
+Napisati program koji racuna koliko u nizu a[n] ima parnih brojeva 
+vecih od broja p
+*/

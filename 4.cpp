33bbce37480d5d06474a9e5e819a8acd67ae358a
Danin Sadzak <daninsadzak@gmail.com>
2023-03-19 13:37:09 +0100

    Napisana postavka za cetvrti

--- 4.cpp
+++ 4.cpp
@@ -4,3 +4,8 @@ using namespace std;
 int main() {
     return 0;
 }
+/*
+Napisati program koji od datog niza a[2n] formira nizove b[n] i c[n]
+tako da su elementi niza b: a[1], a[3], a[5],..., a[2n-1],
+a niza c: a[0], a[2], a[4], a[6],..., a[2n]
+*/

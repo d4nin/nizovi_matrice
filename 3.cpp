4677d87bee7cdf2410b5d415e380d5560b2d68a0
Danin Sadzak <daninsadzak@gmail.com>
2023-03-19 13:32:44 +0100

    Zavrsen treci (tacan)

--- 3.cpp
+++ 3.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Koliki niz?\n";
+    cin >> n;
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    int br(0);
+    int prev = niz[0];
+    for(int i = 0; i < n; i++) {
+        if(prev == niz[i]) br++;
+        prev = niz[i];
+    }
+    cout << "Broj ponavljanja svake cifre: " << br;
     return 0;
 }
+/*
+Ispisati broj ponavljanja svake cifre u datom broju n
+*/

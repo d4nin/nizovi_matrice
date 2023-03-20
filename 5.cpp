7d966ea13370006ad7831912249994592d9ce2ec
Danin Sadzak <daninsadzak@gmail.com>
2023-03-20 23:20:24 +0100

    Napravljen peti zadatak

---
+++ 5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Napisati program koji na osnovu datog niza a[n], formira niz b[n],
+tako da vazi:
+b[i] = a[i] / a[i+1]
+*/

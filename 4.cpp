d5b6e718212b43a54a7033658ac544f682cb6bfa
Danin Sadzak <daninsadzak@gmail.com>
2023-03-20 23:12:54 +0100

    Zavrsen cetvrti zadatak

--- 4.cpp
+++ 4.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
+    int n; 
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int a[2*n];
+    cout << "Unos elemenata niza:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "Niz b:\n";
+    int b[n];
+    for(int i = 0; i < n; i++) {
+        b[i] = a[i]*2-1;
+        cout << b[i] << '\n';
+    }
+    cout << "Niz c:\n";
+    int c[n];
+    for(int i = 0; i < n; i++) {
+        c[i] = a[i] * 2;
+        cout << c[i] << '\n';
+    }
     return 0;
 }
 /*

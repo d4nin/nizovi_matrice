5f8effa827221fb8fd9a473f322e8bd28a2c669e
Danin Sadzak <daninsadzak@gmail.com>
2023-03-20 23:28:26 +0100

    Zavrsen peti zadatak

--- 5.cpp
+++ 5.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    float a[n];
+    cout << "Unos elemenata niza:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "Niz b:\n";
+    float b[n];
+    for(int i = 0; i < n; i++) {
+        if(a[i] != 1) {
+            b[i] = a[i] / (a[i] - 1);
+            cout << b[i] << '\n';
+        }
+    }
     return 0;
 }
 /*

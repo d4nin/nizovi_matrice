b19b45b7b16a5d1b7fe5664555e3c43a17d3b4e1
Danin Sadzak <daninsadzak@gmail.com>
2023-03-20 23:35:47 +0100

    Sesti zadatak uradjen

--- 6.cpp
+++ 6.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    float a[2*n];
+    cout << "Unos clanova niza:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "Niz b:\n";
+    float b[n];
+    for(int i = 0; i < n; i++) {
+        b[i] = (a[i] + (a[i] + 1) - (a[i] - 2*n - 1)) / 2;
+        cout << b[i] << '\n';
+    }
     return 0;
 }
 /*

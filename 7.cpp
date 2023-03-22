75fdc90ffd49bb203aaba1e08ef7d0a460a099ee
Danin Sadzak <daninsadzak@gmail.com>
2023-03-23 00:55:02 +0100

    Sedmi zadatak je zavrsen (valjda je tacan)

--- 7.cpp
+++ 7.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main() {
     int n;
-    cout << "Ucitaj niz!\n";
+    cout << "Enter array size: ";
     cin >> n;
     int a[n], s[n];
-    
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++) {
+        int suma = 0;
+        for(int j = 0; j < n; j++) {
+            if(j != i) {
+                suma += a[j];
+            }
+        }
+        if(n > 1){
+            s[i] = round((double)suma / (n - 1));
+        } else {
+            s[i] = 0;
+        }
+        cout << s[i] << " ";
+    }
     return 0;
 }
+
+
 /*
 Na osnovu niza a[n], formirati niz s[n] čiji se element s[j] dobija
 kao srednja vrijednost niza a iz koga je izbačen element na j-toj 

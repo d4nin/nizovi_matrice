d709ad9f4b2872fa6b42a72a688404761848d77c
Danin Sadzak <daninsadzak@gmail.com>
2023-03-23 00:28:42 +0100

    Napravljen sedmi zadatak

---
+++ 7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Ucitaj niz!\n";
+    cin >> n;
+    int a[n], s[n];
+    
+    return 0;
+}
+/*
+Na osnovu niza a[n], formirati niz s[n] čiji se element s[j] dobija
+kao srednja vrijednost niza a iz koga je izbačen element na j-toj 
+poziciji.
+*/

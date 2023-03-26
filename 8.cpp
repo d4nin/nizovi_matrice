e7d021f73f07f7fdd4fe57cc94a3277281aace0b
Danin Sadzak <daninsadzak@gmail.com>
2023-03-26 01:10:20 +0100

    Napravljen osmi zadatak

---
+++ 8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+
+/*
+Dato je n intervala pomocu nizova a i b:
+(a[0], b[0]), (a[1], b[1]), (a[2], b[2]), (a[n-1], b[n-1]).
+Formirati niz c u kome se nalaze indeksi intervala kojima
+pripada tacka C, odrediti koji od njih je najkraci i ispisati ga. 
+*/

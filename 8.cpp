6a4d54fcd3ef1b7d366483d55e31e93080cbfe20
Danin Sadzak <daninsadzak@gmail.com>
2023-03-26 01:20:06 +0100

    Uradjen osmi zadatak

--- 8.cpp
+++ 8.cpp
@@ -2,6 +2,41 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Koliki niz?\n";
+    cin >> n;
+    int a[n],b[n];
+    cout << "Unesi elemente niza a!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "Unesi elemente niza b!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    int m;
+    cout << "Unesite broj tacaka!\n";
+    cin >> m;
+    int c[m];
+    cout << "Unesite tacke:\n";
+    for(int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+        for(int j = 0; j < n; j++) {
+            if(x >= a[i] && x <= b[i]) {
+                c[i] = j;
+            }
+        }
+    }
+    int najkraci = c[0];
+    for (int i = 1; i < m; i++) {
+        if (b[c[i]] - a[c[i]] < b[najkraci] - a[najkraci]) {
+            najkraci = c[i];
+        }
+    }
+
+    cout << "Najkraci interval je (" << a[najkraci] << ", " << b[najkraci] << ")" << endl;
+
     return 0;
 }
 

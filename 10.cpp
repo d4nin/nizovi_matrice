846dc87a31931ff1641bc7651305fc000948ebc2
Danin Sadzak <daninsadzak@gmail.com>
2023-03-28 23:48:54 +0200

    Zavrsen 10. zadatak

--- 10.cpp
+++ 10.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int main() {
-
+    int m;
+    cout << "Velicina niza?\n";
+    cin >> m;
+    int a[m], b[m];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < m; i++) {
+        cin >> a[i];
+    }
+    int n;
+    cout << "Koliko mjesta?\n";
+    cin >> n;
+    for(int i = 0; i < m; i++) {
+        b[(i + m - n)%m] = a[i];
+    }
+    cout << "Niz b cirkularno pomjeren u lijevo za n mjesta:\n";
+    for(int i = 0; i < m; i++) {
+        cout << b[i] << " ";
+    }
     return 0;
 }
 /*

f8e73201bcce687be51f13624fb8c9c159e98bdd
Danin Sadzak <daninsadzak@gmail.com>
2023-03-28 23:23:39 +0200

    Napravljen 10. zadatak

---
+++ 10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji na osnovu unetog niza a formira niz b koji sadrzi
+sve elemente niza a cirkularno pomjerene u lijevo za n mjesta.
+*/

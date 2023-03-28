8c8ebcf8696fb707ea29b7c96ffc6caa11c164eb
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 00:38:36 +0200

    Zavrsen 12. zadatak

--- 12.cpp
+++ 12.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    cout << "Unesite elemente niza!\n";
+    int a[n];
+    vector<int> b;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int C;
+    cout << "Unesite cifru C!\n";
+    cin >> C;
+    cout << "Niz b:\n";
+    for(int i = 0; i < n; i++) {
+        if((a[i] == C) || (a[i] % 10 == C)) b.push_back(a[i]);
+    }
+    for(int i = 0; i < b.size(); i++) {
+        cout << b[i] << " ";
+    }
     return 0;
 }
 /*

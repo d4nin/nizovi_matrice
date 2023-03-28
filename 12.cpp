959ba5f8fc540d224bbe65a29597fdd9d742b227
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 00:28:40 +0200

    Postavljen 12. zadatak

---
+++ 12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji na osnovu unjetog niza a, formira niz b sastavljen
+od elemenata niza a koji sadrze cifru C.
+*/

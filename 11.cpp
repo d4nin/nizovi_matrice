00614f7ef3a845697861886552ed0377d28357c0
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 00:19:22 +0200

    Napravljen 11. zadatak

---
+++ 11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji za osnovu unjetog niza a formira niz b sastavljen
+od elemenata niza a kojima su cifre poredane obrnutim redoslijedom.
+*/

6d6e499de863b4e37c5702204e73d7a4d60fb412
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 22:41:31 +0200

    Postavljen 18. zadatak

---
+++ 18.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Ako su dva polinoma P(x) i Q(x) dati nizom svojih koefcijenata, napisati program koji racuna
+njihov zbir i proizvod pomocu odgovarajucih funkcija.
+*/

85c5ffbbc5290e31f3217c7456ab7d75c9057583
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 22:28:23 +0200

    Zavrsen 16. zadatak

--- 16.cpp
+++ 16.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 
 int main() {
-     
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    int x,y;
+    cout << "Unesite x i y!\n";
+    cin >> x >> y;
+    int i;
+    while(i < n && niz[i] != x) i++;
+    if(i == n) cout << "Vrijednost x se ne pojavljuje u nizu!\n";
+    int j = i + 1;
+    while(j < n && niz[j] != y) j++;
+    if(j == n) cout << "Vrijednost j se ne pojavljuje u nizu!\n";
+    int p(1);
+    for(int k = i + 1; k < j; k++) p *= niz[k];
+    cout << "Proivod elemenata: " << p;
     return 0;
 }
 /*

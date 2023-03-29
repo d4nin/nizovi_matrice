e9e0eb3c0960cdcb034c82dde80021d5eb0911da
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 22:32:42 +0200

    Postavljen 17. zadatak

---
+++ 17.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Ako je polinom P(x) = a0 + a1x + a2x^2 +...+ anx^n dat nizom svojih koefcijenata, napisati
+program koji izracunava vrijednost polinoma za dato x.
+*/

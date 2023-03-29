3fbe0e42718f18c2c8a2510a7cd6c493adff940d
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 22:21:46 +0200

    Postavljen 16. zadatak

---
+++ 16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     
+    return 0;
+}
+/*
+Korisnik unosi celobrojni niz a i celobrojne vrijednosti x i y.
+Odrediti proizvod elemenata u nizu a koji se nalaze izmedu prve pojave
+vrijednosti x u nizu i prve pojave vrijednost y u nizu
+*/

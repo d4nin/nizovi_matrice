68d370b212126e00bd3ebaea121c7ac7f8a9c80c
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 22:36:30 +0200

    Zavrsen 17. zadatak

--- 17.cpp
+++ 17.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int x[n];
+    cout << "Unesite elemente niza x!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+    int P(0);
+    for(int i = 0; i < n; i++) {
+        P += i + (i * pow(x[i],i));
+    }
+    cout << "Vrijednost polinoma P: " << P;
     return 0;
 }
 /*

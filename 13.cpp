5ec81be4c5f8fee3902285993ef3667a86d4fe47
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 11:00:15 +0200

    Zavrsen 13. zadatak

--- 13.cpp
+++ 13.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int a[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int br(0);
+    for(int i = 0; i < n; i++) {
+        if(a[i] * a[i-1] < 0) br++;
+        if(a[i] == 0) continue;
+    }
+    cout << "Broj promjena znaka:\n" << br;
     return 0;
 }
 /*

ed15f2c9264d26b597704fa14f8a1bdc52e56f88
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 11:02:49 +0200

    Postavljen 14. zadatak

---
+++ 14.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Pomocu funkcije provjera provjeriti da li su svaka dva susjedna elementa
+niza a razlicita. Napati glavni program u kome ce se unositi niz a i
+provjeravati da li su susjedni elementi razliciti.
+*/

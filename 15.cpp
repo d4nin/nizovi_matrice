ddd638272e157cb379db0c917d8b50290dddf73f
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 11:28:45 +0200

    Zavrsen 15. zadatak

--- 15.cpp
+++ 15.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cout << "Velicina niza?\n";    
+    cin >> n;
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    cout << "Broj P?\n";
+    int P, z(0);
+    cin >> P;
+    for(int i = 0; i < n; i++) {
+        if(niz[i] != P) z += niz[i];
+    }
+    cout << "Zbir elemenata:\n" << z;
     return 0;
 }
 /*

41d99b1aa4442726270e51ea06ac4981cefc8a39
Danin Sadzak <daninsadzak@gmail.com>
2023-03-29 11:21:04 +0200

    Postavljen 15. zadatak

---
+++ 15.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Dat je niz realnih brojeva. Potrebno je odrediti zbir elemenata koji
+prethode prvom pojavljivanju datog realnog broja P u nizu.
+*/

61bc55589eb80119983d443332afcffb1caad127
Danin Sadzak <daninsadzak@gmail.com>
2023-03-30 23:04:24 +0200

    Zavrsen 19. zadatak

--- 19.cpp
+++ 19.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void sort(int n) {
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    bool lijevo = true, desno = true;
+    for(int i = 1; i < n; i++) {  
+        if(niz[i] < niz[i-1]) lijevo = false; 
+        if(niz[i] > niz[i-1]) desno = false; 
+    }
+    if(lijevo) cout << "Niz je sortiran sa lijeva na desno.";
+    else if(desno) cout << "Niz je sortiran sa desna na lijevo.";
+    else cout << "Niz nije sortiran.";
+}
+
 int main() {
-    
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    sort(n);
     return 0;
 }
 /*

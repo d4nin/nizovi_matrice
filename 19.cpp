d3c776bbe1324687bea6548fd7eedccdc0435644
Danin Sadzak <daninsadzak@gmail.com>
2023-03-30 22:19:47 +0200

    Postavljen 19. zadatak

---
+++ 19.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Napisati funkciju koja provjerava da li je niz sortiran i, ako jeste, ispisuje kojim je
+redoslijedom sortiran.
+*/

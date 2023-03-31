1574da6bc1190943feba66d074b0f5c9daa29593
Danin Sadzak <daninsadzak@gmail.com>
2023-03-31 17:39:06 +0200

    Postavljen 27. zadatak

---
+++ 27.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Na osnovu unjetog niza a, formirati niz b koji se sastoji iz prostih clanova niza a sortiranih u
+opadajucem poretku.
+*/

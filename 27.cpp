781ddeb8390e17567601bb75b515bd96f37f974b
Danin Sadzak <daninsadzak@gmail.com>
2023-03-31 23:00:09 +0200

    Zavrsen 27. zadatak

--- 27.cpp
+++ 27.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+bool prosti(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+int main() {
+    int n; 
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int a[n], b[n];
+    int br(0);
+    cout << "Unesite clanove niza a!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        if(prosti(a[i])) {
+            b[br] = a[i];
+            br++;
+        }
+    }
+    for(int i = 0; i < br; i++) {
+        for(int j = 0; j < br - 1 - i; j++) {
+            if(b[j] > b[j + 1]) {
+                int temp = b[j + 1];
+                b[j + 1] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+    cout << "Sortirani prosti brojevi:\n";
+    for(int i = br - 1; i >= 0; i--) {
+        cout << b[i] << " ";
+    }
     return 0;
 }
 /*

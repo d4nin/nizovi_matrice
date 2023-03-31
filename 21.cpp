8ee65248fa9e1e40ebd1239e45b05dee4d6adb7f
Danin Sadzak <daninsadzak@gmail.com>
2023-03-31 08:20:49 +0200

    Zavrsen 21. zadatak

--- 21.cpp
+++ 21.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void SelectionSort(int n) {
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    int tmin(0);
+    for(int i = 0; i < n - 1; i++) {
+        tmin = i;
+        for(int j = i + 1; j < n; j++) {
+            if(niz[j] < niz[tmin]) {
+                tmin = j;
+            }
+        }
+        if(tmin != i) {
+            swap(niz[i], niz[tmin]);
+        }
+    }
+    cout << "Sortiran niz:\n";
+    for(int i = 0; i < n; i++) {
+        cout << niz[i] << " ";
+    }
+}
 
+int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int niz[n];
+    SelectionSort(n);
     return 0;
 }
 /*

6008888db7b1a5a87e429f2e9db865e15f035bd2
Danin Sadzak <daninsadzak@gmail.com>
2023-03-31 09:26:01 +0200

    Postavljen 26. zadatak

---
+++ 26.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji unjeti niz a sortira tako da elemnti sa parnim indeksima budu sortirani u
+rastucem, a na neparnim u opadajucem poretku
+*/

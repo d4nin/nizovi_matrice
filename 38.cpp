e5578b7cbb673d38767ab353eda5b84f40830699
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 19:19:24 +0200

    Zavrsen 38. zadatak

--- 38.cpp
+++ 38.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void najmanjiNajveci(int n, int m) {
+    int mat[n][m];
+    int tmax(0), tmin(n);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesite broj na indexu [" << i << "," << j << "]: ";
+            cin >> mat[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(tmax < mat[i][j]) tmax = mat[i][j];
+            if(tmin > mat[i][j]) tmin = mat[i][j];
+        }
+    }
+    cout << "Najveci element: " << tmax << '\n';
+    cout << "Najmanji element: " << tmin; 
+ }
 
+int main() {
+    int n,m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    najmanjiNajveci(n,m);
     return 0;
 }
 /*

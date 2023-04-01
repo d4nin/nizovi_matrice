a349b7f0d8d4f9ce083796fe72033a8a0ceb54a3
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 01:25:53 +0200

    Zavrsen 45. zadatak

--- 45.cpp
+++ 45.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina matrice?\n";
+    cin >> n;
+    int a[n][n], b[n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Unesite broj na indexu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        int p = 1;
+        for(int j = 0; j < n; j++) {
+            p *= a[i][j];
+        }
+        b[i] = p;
+    }
+    cout << "Niz b:\n";
+    for(int i = 0; i < n; i++) {
+        cout << b[i] << " ";
+    }
     return 0;
 }
 /*

787c0ee144d1c5893814d29c21ef72d17d59070e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 19:11:25 +0200

    Postavljen 38. zadatak

---
+++ 38.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Data je matrica prirodnih brojeva dimenzie NxM. Napisati funkciju koja u matrici odredjuje najveci i najmanji element
+*/

3072e7b5b20d6c72be3906a1aa91250b619db098
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 15:46:02 +0200

    Postavljen 31. zadatak

---
+++ 31.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Sjaj zvijedzda (i nebeskih tijela) iskazuje se u magnitudama. Sto je tjelo sjajnije, to je magnituda manja (sjaj
+Sunca je -26.8, pug Mjeseca -24.6, najsjajnije zvijezde na nebu, Sirijusa -1.4, a nesto manje sjajne zvijezde Deneb
+1.25). U nizu a[n] nalaze se magnitude n nebeskih tijela. Potrebno ga je ispisati sortiranog od najsjajnijeg ka 
+najmanjem sjajnijem tijelu i ispisati 4 najsjajnija objekta.
+*/

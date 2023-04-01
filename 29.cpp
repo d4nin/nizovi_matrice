672c6ec9eef3128d6990e9186103f3f21aef19ba
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 14:28:00 +0200

    Postavljen 29. zadatak

---
+++ 29.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funckiju koja istovremeno racuna aritmeticku sredinu parnih clanova i aritmeticku sredinu prostih clanova
+unjetog niza a.
+*/

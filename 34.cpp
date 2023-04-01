83a3e33f2fa27282044aabce5e359c9c787aefb8
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 18:30:56 +0200

    Zavrsen 34. zadatak

--- 34.cpp
+++ 34.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void transp(int n, int m) {
+    int mat[n][m];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesite broj na poziciji (" << i << "," << j << "): ";
+            cin >> mat[i][j];
+        }
+    }
+    int trans[m][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            trans[j][i] = mat[i][j];
+        }
+    }
+    cout << "Transponovana matrica:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << trans[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    
+    int n, m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    transp(n,m);
     return 0;
 }
 /*

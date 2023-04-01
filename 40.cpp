10546183a706987062faa7b67be564f743c0e6db
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 19:49:12 +0200

    Zavrsen 40. zadatak

--- 40.cpp
+++ 40.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+bool simetrican(int n) {
+    int mat[n][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Unesi broj na indexu [" << i << "," << j << "]: ";
+            cin >> mat[i][j]; 
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i != j && mat[i][j] != mat[j][i]) return false;
+        }
+    }
+    return true;
+}
 
+int main() {
+    int n;
+    cout << "Velicina matrice?\n";
+    cin >> n;
+    if(simetrican(n)) cout << "Matrica je simetricna.";
+    else cout << "Matrica nije simetricna.";
     return 0;
 }
 /*

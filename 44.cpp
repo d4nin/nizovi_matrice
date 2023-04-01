1e81faf85e1a60e88ce37de758d998717bc609f2
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 01:15:33 +0200

    Zavrsen 44. zadatak

--- 44.cpp
+++ 44.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+bool prost(int niz) {
+    if(niz <= 1) return false;
+    for(int i = 2; i < niz; i++) {
+        if(niz % i == 0) return false;
+    }
+    return true;
+}
 
+int main() {
+    int n,m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    int a[n][m], b[m];
+    int br(0);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesite broj na indeksu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+            if(prost(a[i][j])) {
+                b[br] = a[i][j];
+                br++;
+            }
+        }
+    }
+    for(int i = 0; i < br; i++) {
+        for(int j = 0; j < br - 1 - i; j++) {
+            if(b[j] > b[j + 1]) {
+                int temp = b[j + 1];
+                b[j + 1] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+    cout << "Niz b:\n";
+    for(int i = 0; i < br; i++) {
+        cout << b[i] << " ";
+    }
     return 0;
 }
 /*

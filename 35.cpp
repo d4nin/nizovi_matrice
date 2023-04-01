d3e32c79872d9e03f01adce3f52a4023d218c598
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 18:36:04 +0200

    Postavljen 35. zadatak

---
+++ 35.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Za datu kvadratnu matricu a[n][m], napisati program koji pomocu odgovarajucih funkcija racuna:
+a. zbir elemenata na glavnoj dijagonali
+b. zbir elemenata na sporednoj dijagonali
+c. zbir elemenata iznad glavne dijagonale 
+d. zbir elemenata ispod sporedne dijagonale
+*/

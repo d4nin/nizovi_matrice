924f10abba4fca04913ea1fecddd3b9ebd075ab9
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 15:58:33 +0200

    Zavrsen 31. zadatak

--- 31.cpp
+++ 31.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Koliko nebeskih tijela?\n";
+    cin >> n;
+    float a[n];
+    cout << "Unesite magnitude nebeskih tijela!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - 1 - i; j++) {
+            if(a[j] > a[j + 1]) {
+                float temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp; 
+            }
+        }
+    }
+    if(n < 4) {
+        cout << "Najsjajniji objekti:\n";
+        for(int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+    }
+    cout << "4. najsjajnija objekata:\n";
+    for(int i = 0; i < 4; i++) {
+        cout << a[i] << " ";
+    }
     return 0;
 }
 /*

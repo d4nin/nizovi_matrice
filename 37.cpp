fc44d9e29db648a557efb6e5794ff50435c28792
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 18:57:39 +0200

    Postavljen 37. zadatak

---
+++ 37.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program kojim se na osnovu kvadratne matrice a formira matrica b tako da su joj sporedna i glavna dijagonala
+jednake sporednoj i glavnoj dijagonali matrice a, dok su ostali elementi jednaki 0
+*/

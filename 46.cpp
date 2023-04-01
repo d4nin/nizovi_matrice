5d8a966b3c2ff2bac40426d4b8c1312ecc2557a5
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 01:27:50 +0200

    Postavljen 46. zadatak

---
+++ 46.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Korisnik unosi matricu a[n][m]. Pronaci koliko elemenata u matrici su istovremeno najveci u svom redu i najmanji u 
+svojoj koloni i ispisati njihove indexe
+*/

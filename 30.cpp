0a703b6e7925d78f85ff700a3298523495f1a2da
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 14:59:20 +0200

    Zavrsen 30. zadatak

--- 30.cpp
+++ 30.cpp
@@ -2,7 +2,47 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    int br, index;
+    cout << "Unesite broj za umetanje i na kojem indexu!\n";
+    cin >> br >> index;
+    if(index > n) {
+        niz[n] = br;
+        n++;
+        cout << "Niz sa umetnutim brojem:\n";
+        for(int i = 0; i < n; i++) {
+            cout << niz[i] << " ";
+        }
+    }
+    else if(index < 0) {
+        for(int i = n - 1; i >= 0; i--) {
+            niz[i + 1] = niz[i];
+        }
+        niz[0] = br;
+        n++;
+        cout << "Niz sa umetnutim brojem:\n";
+        for(int i = 0; i < n; i++) {
+            cout << niz[i] << " ";
+        }
+    }
+    else { 
+        for(int i = n; i > index; i--) {
+            niz[i] = niz[i - 1];
+        }
+        niz[index] = br;
+        n++;
+        cout << "Niz sa umetnutim brojem:\n";
+        for(int i = 0; i < n; i++) {
+            cout << niz[i] << " ";
+        }
+    }
     return 0;
 }
 /*

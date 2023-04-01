bc568cef193eb7173b8f9f5be8ef48241dbb513d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 21:05:08 +0200

    Zavrsen 42. zadatak

--- 42.cpp
+++ 42.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void zamjeni_red(int n, int m) {
+    int a[n][m];
+    int k, p;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesite broj na indexu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+    cout << "Unesite red k i p:\n";
+    cin >> k >> p;
+    k--;
+    p--;
+    if(k >= 0 && k < n && p >= 0 && p < n) {
+        for(int j = 0; j < m; j++) {
+            int temp = a[k][j];
+            a[k][j] = a[p][j];
+            a[p][j] = temp;
+        }
+        cout << "Nova matrica:\n";
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "Neispravan unos za k i p.\n";
+    }
+}
 
+int main() {
+    int n, m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    zamjeni_red(n,m);
     return 0;
 }
 /*

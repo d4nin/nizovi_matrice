82714696b2b85e8fb390903b1104d1821ced2931
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 19:23:45 +0200

    Postavljen 39. zadatak

---
+++ 39.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Data je matrica prirodnih brojeva dimenzije NxM. Napisati funkciju koja u matrici odredjuje zbir elemenata sa 
+neparnim indeksom i i neparnim indeksom j
+*/

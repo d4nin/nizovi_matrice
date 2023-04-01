66d0cb1c341bafca756c1e049d3a2cf197e4c95b
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 16:38:54 +0200

    Postavljen 34. zadatak

---
+++ 34.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkciju "transp" koja formira transponovanu matricu i glavni program koji je testira za unjetu matricu
+*/

3175079d3178fe8d756a1f3fef8fb0d631e6bfaa
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 18:48:10 +0200

    Postavljen 36. zadatak

---
+++ 36.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Napisati program koji na osnovu unjete matrice a[n][m] formira matricu b[n][m] ciji su elementi ili 1 ili 0 ili to,
+ako je element a[i][j] paran tada je b[i][j] jednak 0, a ako je neparan b[i][j] je jednak 1.
+*/

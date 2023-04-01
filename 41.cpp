ae1ac967a226ae9d4896323458bcc9aac2cd278d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 19:50:57 +0200

    Postavljen 41. zadatak

---
+++ 41.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkciju koja mijenja matricu a tako sto k-ti red zamjenjuje odgovarajucim elementima niza b
+*/

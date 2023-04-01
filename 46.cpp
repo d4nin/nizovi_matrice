53b8fa97f8720835ceccb2f79bad8fa40250ff4e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 01:46:46 +0200

    Zavrsen 46. zadatak

--- 46.cpp
+++ 46.cpp
@@ -2,6 +2,39 @@
 using namespace std;
 
 int main() {
+    int n,m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    int a[n][m];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesite broj na indexu: [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+    int maxRed[n], minKolona[m];
+    int tmin(0), tmax(0);
+    for(int i = 0; i < n; i++) {
+        tmax = a[i][0];
+        for(int j = 0; j < m; j++) {
+            if(tmax < a[i][j]) tmax = a[i][j];
+        }
+        maxRed[i] = tmax;
+    }
+    for(int i = 0; i < n; i++) {
+        tmin = a[0][i];
+        for(int j = 0; j < m; j++) {
+            if(tmin > a[i][j]) tmin = a[i][j];
+        }
+        minKolona[i] = tmin;
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(a[i][j] == maxRed[i] && a[i][j] == minKolona[j]) {
+                cout << "a[" << i << "][" << j << "] = " << a[i][j] << "\n"; 
+            }
+        }
+    }
     return 0;
 }
 /*

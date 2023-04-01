30b43499f885be9fb5314066e22d1178fdeffb0e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 14:26:01 +0200

    Zavrsen 28. zadatak

--- 28.cpp
+++ 28.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() { 
+void najmanjaNajveca(int n) {
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+    int tmin(n), tmax(0);
+    for(int i = 0;  i < n; i++) {
+        if(tmax < niz[i]) tmax = niz[i];
+        if(tmin > niz[i]) tmin = niz[i];
+    }
+    cout << "Najmanja vrijednost niza: " << tmin << '\n';
+    cout << "Najveca vrijednost niza: " << tmax; 
+}
 
+int main() { 
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    najmanjaNajveca(n);
     return 0;
 }
 /*

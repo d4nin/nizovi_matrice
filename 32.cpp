7e34217dee93f71fedffc54553807656a865f981
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 16:05:26 +0200

    Zavrsen 32. zadatak

--- 32.cpp
+++ 32.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina kvadratne matrice?\n";
+    cin >> n;
+    int mat[n][n];
+    int z(0);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Unesite broj na poziciji (" << i << "," << j <<"): ";
+            cin >> mat[i][j];
+            z += mat[i][j];
+        }
+    }
+    cout << "Zbir elemenata kvadratne matrice: " << z;
     return 0;
 }
 /*

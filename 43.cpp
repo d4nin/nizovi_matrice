8b9d31dda8ef67aa6fe694adb9af8ef977852c15
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 00:55:59 +0200

    Postavljen 43. zadatak

---
+++ 43.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Formirati niz b od elemenata kvadratne matrice a[n][m] koji predstavljaju najvece vrijednosti odgovarajuceg reda
+matrice i niz c koji sadrzi najmanje vrijednosti odgovarajuceg reda matrice a
+*/

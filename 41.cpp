8a5183a81ee5677f512c9b8e109d72b16fa4f9d4
Danin Sadzak <daninsadzak@gmail.com>
2023-04-01 20:40:40 +0200

    Zavrsen 41. zadatak

--- 41.cpp
+++ 41.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void zamjeniRed(int n) {
+    int a[n][n];
+    int b[n], k;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Unesite broj na indeksu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+    cout << endl;
+    cout << "Unesite niz b:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    cout << "Unesite red za zamjenu:\n";
+    cin >> k;
+    for(int j = 0; j < n; j++) {
+        a[k][j] = b[j];
+    }
+    cout << "Nova matrica:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
+int main() {
+    int n;
+    cout << "Velicina matrice?\n";
+    cin >> n;
+    zamjeniRed(n);
     return 0;
 }
 /*

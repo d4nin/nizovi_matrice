64b2d3f819772c969e2601f6944138e24beb5bf3
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 15:00:27 +0200

    Zavrsen 49. zadatak

--- 49.cpp
+++ 49.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int a[n], p(1);
+    cout << "Unesite elemente niza a!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        p *= a[i];
+    }
+    cout << "Proizvod elemenata niza a: " << p;
     return 0;
 }
 /*

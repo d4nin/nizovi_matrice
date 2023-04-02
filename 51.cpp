8380154d60b39373c59c42b30b53d7d84d71acef
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 15:14:15 +0200

    Zavrsen 51. zadatak

--- 51.cpp
+++ 51.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Unesite broj N: ";
+    cin >> n;
+
+    // brojanje pojavljivanja cifara
+    int count[10] = {0};
+    while(n != 0) {
+        int digit = n % 10;
+        count[digit]++;
+        n /= 10;
+    }
+
+    // pronalazak najcesce ponavljajuce cifre
+    int max_count = -1, max_digit = -1;
+    for(int i = 0; i < 10; i++) {
+        if(count[i] > max_count) {
+            max_count = count[i];
+            max_digit = i;
+        }
+    }
+
+    // ispisivanje rezultata
+    cout << "Cifra " << max_digit << " se najvise puta ponovila (" << max_count << " puta)." << endl;
 
     return 0;
 }

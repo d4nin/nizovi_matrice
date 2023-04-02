102a0930b471bcfb1354732ce9e3820ba74f9921
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 14:45:28 +0200

    Postavljen 48. zadatak

---
+++ 48.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkciju lagranz koja koristeci langrazov interpolacioni polinom izracunava vrijednost funkcije f(x) za datu
+vrijednost x. U glavnom programu se unosi niz vrijednosti x i niz odgovarajucih vrijednosti f(x). Kao rezultat se
+ispisuje vrijednost funkcije za vrijednost koji unosi korisnik.
+*/

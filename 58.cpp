0ed35365aead95d8d69cf4777a100108cff1d1ba
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 23:48:38 +0200

    Postavljen 58. zadatak

---
+++ 58.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Za njetu kvadratnu matricu a, ispisati zbir svakih neparnih elemenata koji se nalaze na mjestima gdje je zbir indeksa
+i i j jednak datom broju k, to jest i+j=k
+*/

b1487eb63fd3d0261776b340654f7a7613528d5b
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:06:52 +0200

    Postavljen 60. zadatak

---
+++ 60.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Potrebno je popuniti kvadratnu kvadratnu matricu a na zadati nacin. Npr. za unjetu dimenziju 4 treba formirati i 
+ispisati matricu oblika:
+1 2 3 4
+2 3 4 1
+3 4 1 2
+4 1 2 3
+*/

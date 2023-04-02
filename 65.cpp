0253116c47ab72ce107d5f4fae5044135ae07965
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:56:47 +0200

    Postavljen 65. zadatak

---
+++ 65.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Data je matrica a[n][m]. Na osnovu nje formirati niz d[n] gdje je d[0] zbir elemenata u prvom redu, d[1] zbir
+elemenata u drugom redu itd
+*/

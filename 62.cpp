0e4987dabaa157205f1d95202c353f6693f2c6b5
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:33:59 +0200

    Zavrsen 62. zadatak

--- 62.cpp
+++ 62.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
-
+    int n, m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    int a[n][m];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesite broj na indexu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0) {
+            sort(a[i], a[i] + m);
+        } else {
+            sort(a[i], a[i] + m, greater<int>());
+        }
+    }
+    cout << "Izmjenjena matrica:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 /*

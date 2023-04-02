a781a545caac79adf259590a3217df33969ab976
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:45:46 +0200

    Postavljen 64. zadatak

---
+++ 64.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Formirati niz b koji se sastoji od elemenata na sporednoj dijagonali matrica a i ispisati ga sortiranog u rastucem
+poretku.
+*/

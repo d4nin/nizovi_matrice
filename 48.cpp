e2af029c2e481e321815c5d974fa50235390b6fb
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 14:55:12 +0200

    Zavrsen 48. zadatak

--- 48.cpp
+++ 48.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+double lagrange(double x, double y[], double f[], int n);
 
+int main() {
+    int n;
+    cout << "Unesite broj tacaka: ";
+    cin >> n;
+    double x[n], f[n];
+    for(int i=0; i<n; i++) {
+        cout << "Unesite x_" << i << ": ";
+        cin >> x[i];
+        cout << "Unesite f(x_" << i << "): ";
+        cin >> f[i];
+    }
+    double x_value;
+    cout << "Unesite vrijednost x za koju zelite izracunati f(x): ";
+    cin >> x_value;
+    double result = lagrange(x_value, x, f, n);
+    cout << "f(" << x_value << ") = " << result << endl;
     return 0;
+}
+
+double lagrange(double x, double y[], double f[], int n) {
+    double result = 0;
+    for(int i=0; i<=n; i++) {
+        double term = f[i];
+        for(int j=0; j<=n; j++) {
+            if(j != i) {
+                term *= (x - y[j])/(y[i] - y[j]);
+            }
+        }
+        result += term;
+    }
+    return result;
 }
 /*
 Napisati funkciju lagranz koja koristeci langrazov interpolacioni polinom izracunava vrijednost funkcije f(x) za datu

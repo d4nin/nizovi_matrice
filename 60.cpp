cb9984e8ee26c2b9a10536fa87639952bbea4d93
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:11:00 +0200

    Zavrsen 60. zadatak

--- 60.cpp
+++ 60.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina matrice?\n";
+    cin >> n;
+    int mat[n][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            mat[i][j] = (i + j) % n + 1;
+        }
+    }
+    cout << "Ispis matrice:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 /*

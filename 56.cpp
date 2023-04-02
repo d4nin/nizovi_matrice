325c5d1339e1faa52812829b03a4d2ce6db27a09
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 18:27:40 +0200

    Zavrsen 56. zadatak

--- 56.cpp
+++ 56.cpp
@@ -2,7 +2,30 @@
 using namespace std;
 
 int main() {
-
+    int n, m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    int a[n][m];
+    int p[n][m];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesite broj na indexu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+            p[i][j] = a[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            p[i][j] *= a[i][j];
+        }
+    }
+    cout << "Proizvod elemenata matrice:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << p[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 /*

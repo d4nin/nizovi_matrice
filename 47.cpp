497e865b06e8209fe735f5c86ea6e09020dbb5c8
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 14:12:55 +0200

    Postavljen 47. zadatak

---
+++ 47.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji za odgovarajuce N formira i ispisuje kvadratnu matricu sljedeceg oblika, npr za N = 4:
+   1   2   3   4
+  12  13  14   5
+  11  16  15   6
+  10   9   8   7
+*/

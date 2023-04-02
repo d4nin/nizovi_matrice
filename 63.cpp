7305004945fd3dab70ba99cbe99f372c132449c7
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:35:19 +0200

    Postavljen 63. zadatak

---
+++ 63.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkciju koja provjerava da li je kvadratna matrica simetricna u odnosu na sporednu dijagonalu.
+*/

f044d8adb132f274a62e823bd250b3f553ca3b0f
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:21:35 +0200

    Zavren 61. zadatak

--- 61.cpp
+++ 61.cpp
@@ -2,7 +2,28 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina matrice?\n";
+    cin >> n;
+    int a[n][n], b[n][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Unesite broj na indexu: [" << i << "," << j << "]: ";
+            cin >> a[i][j]; 
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            b[i][j] = a[j][n - i - 1];
+        }
+    }
+    cout << "Matrica b:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 /*

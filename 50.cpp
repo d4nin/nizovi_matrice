34d199ae667b915507738ea6e4cd107cb59a5ac0
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 15:10:29 +0200

    Zavrsen 50. zadatak

--- 50.cpp
+++ 50.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int niz[n], br1(0), br2(0);
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+        if(niz[i] > 0) br1++;
+        if(niz[i] < 0) br2++;
+    }
+    cout << "Broj pozitivnih brojeva: " << br1 << '\n';
+    cout << "Broj negativnih brojeva: " << br2; 
     return 0;
 }
 /*

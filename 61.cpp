e3da467d85e27ade94285dbfbbe161b863b4bf8e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:13:03 +0200

    Postavljen 61. zadatak

---
+++ 61.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Na osnovu kvadratne matrice a formirati kvadratnu matricu b koja predstavlja matricu a u ogledalu (krajnja lijeva
+kolona matrice a treba da bude krajnja desna matrica b itd)
+*/

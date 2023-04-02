6690d9f62c1b0cf855427e681f1c686af6811896
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 18:34:40 +0200

    Zavrsen 57. zadatak

--- 57.cpp
+++ 57.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina matrice?\n";
+    cin >> n;
+    int a[n][n], z1(0), z2(0);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Unesite broj na indexu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i > j) z1 += a[i][j];
+            if(i + j < n -1) z2 += a[i][j];
+        }
+    }
+    cout << "Zbir elemenata ispod glavne dijagonale: " << z1 << '\n';
+    cout << "Zbir elemanata iznad sporedne diajagonale: " << z2; 
     return 0;
 }
 /*

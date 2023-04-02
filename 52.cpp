de44babf2d2933a9251bc44ac599203084cb81a8
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 15:16:56 +0200

    Postavljen 52. zadatak

---
+++ 52.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati programkoji na osnovu unjetog niza a formira niz b koji se sastoji iz aritmeticke sredine tri elemenata niza
+a, i to na sljedeci nacin:
+b[i] = (a[i] + a[i+1] + a[i+2])/3
+*/

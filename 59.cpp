db77e538dd43e96149e73bc70977957602d5dd2d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:04:06 +0200

    Zavrsen 59. zadatak

--- 59.cpp
+++ 59.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main() {
-
+    int n, m;
+    cout << "Velicina matrice?\n";
+    cin >> n >> m;
+    int a[n][m], broj, br(0);
+    cout << "Unesite broj!\n";
+    cin >> broj;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Unesi broj na indexu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+            if(a[i][j] == broj) br++;
+        }
+    }
+    cout << "Broj " << broj << " se ponavlja " << br << " puta.";
     return 0;
 }
 /*

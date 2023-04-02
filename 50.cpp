ccfdecf94a276060f0ba8860bf10596d5a6c51ae
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 15:04:04 +0200

    Postavljen 50. zadatak

---
+++ 50.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Ucitati niz pd n elemenata i ispisati koliko u njemu ima negativnih, a koliko pozitivnih brojeva
+*/

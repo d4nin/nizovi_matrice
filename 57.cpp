5bf99b48e1fcc03bb439ab0bfafafe060e73c9a3
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 18:29:35 +0200

    Postavljen 57. zadatak

---
+++ 57.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Za datu kvadratnu matricu a[n][m] napisati program koji pomocu odgovarajucih funkcija racuna:
+a. zbir elemenata ispod glavne dijagonale
+b. zbir elemanata iznad sporedne dijagonale
+*/

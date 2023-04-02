858b71b486f4faa95c943dc04db57776011e7bba
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:42:43 +0200

    Zavrsen 63. zadatak

--- 63.cpp
+++ 63.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+bool simetricaPoSporednoj(int n) {
+    int a[n][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Unesite broj na indexu [" << i << "," << j << "]: ";
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(a[i][j] != a[n - j - 1][n - i - 1]) return false;
+         }
+    }
+    return true;
+}
 
+int main() {
+    int n;
+    cout << "Velicina matrice?\n";
+    cin >> n;
+    if(simetricaPoSporednoj(n)) cout << "Matrica je simetricna u odnosu na sporednu dijagonalu.";
+    else cout << "Matrica nije simetricna u odnosu na sporednu dijagonalu.";
     return 0;
 }
 /*

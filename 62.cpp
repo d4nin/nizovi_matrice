d8bf59156696898a9104c7bb23c8d2cef6454067
Danin Sadzak <daninsadzak@gmail.com>
2023-04-03 00:23:58 +0200

    Postavljen 62. zadatak

---
+++ 62.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji mijenja datu matricu a tako sto sortira elemente u svakom redu i to elemente u prvom redu
+rastucim redoslijedom, elemente u drom opadajucim, elemente u trecem rastucim itd.
+*/

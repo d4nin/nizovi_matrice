13066922666468d254e39c82f5e7cb325637d15e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 23:56:20 +0200

    Postavljen 59. zadatak

---
+++ 59.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji ispisuje koliko se puta odredeni broj pojavio u matrici a[n][m]. Matrica a treba da sadrzi samo
+brojeve izmedju 0 i 9.
+*/

8b6a17edc9181bbda262c568edf6d345ec62b30e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 15:25:04 +0200

    Zavrsen 53. zadatak

--- 53.cpp
+++ 53.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int a[n], b[n], c[n];
+    cout << "Unesite elemente niza a!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "Unesite elemente niza b!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    for(int i = 0; i < n; i++) {
+        c[i] = a[i] + b[n - 1 - i];
+    }
+    cout << "Niz c:\n";
+    for(int i = 0; i < n; i++) {
+        cout << c[i] << " ";
+    }
     return 0;
 }
 /*

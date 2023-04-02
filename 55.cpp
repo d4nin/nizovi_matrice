74c4c17d4ca3b393d57ae4eed665e309dee9ef0e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 18:11:10 +0200

    Postavljen 55. zadatak

---
+++ 55.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkciju koja uklanja sve elemente niza b koji su jednaki datom elementu i kao rezultat vraca izmjenjeni niz
+*/

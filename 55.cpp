dd3902be3ba926023064c93027036af2fdca0ba8
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 18:20:49 +0200

    Zavrsen 55. zadatak

--- 55.cpp
+++ 55.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void ukloniElement(int n) {
+    int b[n], element;
+    int brNepoklapanja(0);
+    cout << "Unesite elemente niza b!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    cout << "Unesite element!\n";
+    cin >> element;
+    for(int i = 0; i < n; i++) {
+        if(b[i] != element) brNepoklapanja++;
+    }
+    int noviB[brNepoklapanja];
+    int j(0);
+    for(int i = 0; i < n; i++) {
+        if(b[i] != element) {
+            noviB[j] = b[i];
+            j++;
+        }
+    }
 
+    cout << "Novi niz\n";
+    for(int i = 0; i < brNepoklapanja; i++) {
+        cout << noviB[i] << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    ukloniElement(n);
     return 0;
 }
 /*

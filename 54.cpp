7497fae36132486fdc3c3522a79cfa69dae27dd1
Danin Sadzak <daninsadzak@gmail.com>
2023-04-02 18:04:59 +0200

    Postavljen 54. zadatak

---
+++ 54.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Na osnovu nizova a[n] i b[n], formirati niz c[n] po sljedecoj formuli:
+c[i] = a[i] + a[n - 1 - i] / 2 + b[i] / b[n - 1 - i]
+*/
